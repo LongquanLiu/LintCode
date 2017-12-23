d082ad7e1ba44379dfa2d114387656a0b0642b60
LongquanLiu <30406150+LongquanLiu@users.noreply.github.com>
2017-12-23 16:28:35 +0800

    Update fizz-buzz.cpp

--- C++/fizz-buzz.cpp
+++ C++/fizz-buzz.cpp
@@ -1,27 +1,35 @@
 // Time:  O(n)
 // Space: O(1)
 
+#include <string>
+#include <vector>
+using namespace std;
 class Solution {
 public:
-    /**
-     * param n: As description.
-     * return: A list of strings.
+    /*
+     * @param n: An integer
+     * @return: A list of strings.
      */
     vector<string> fizzBuzz(int n) {
-        vector<string> result;
-
-        for (int i = 1; i <= n; ++i) {
-            if (i % 15 == 0) {
-                result.emplace_back("fizz buzz");
-            } else if (i % 5 == 0) {
-                result.emplace_back("buzz");
-            } else if (i % 3 == 0) {
-                result.emplace_back("fizz");
-            } else {
-                result.emplace_back(to_string(i));
+        // write your code here
+        vector<string> str;
+        for (int i = 1; i <= n; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                str.emplace_back("fizz buzz");
+            }else if (i % 3 == 0){
+                str.emplace_back("fizz");
+            }else if (i % 5 == 0){
+                str.emplace_back("buzz");
+            }else{
+                str.emplace_back(to_string(i));
             }
         }
-
-        return result;
+        return str;
     }
 };
+
+/* C11
+ * emplace_back能就地通过参数构造对象，不需要拷贝或者移动内存，
+ * 相比push_back能更好地避免内存的拷贝与移动，使容器插入元素的性能得到进一步提升。
+ * 在大多数情况下应该优先使用emplace_back来代替push_back。
+ * */
