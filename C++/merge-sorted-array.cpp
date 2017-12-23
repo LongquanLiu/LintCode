abdcf85ddaa62ef6587ca2fb9c1ee99dc93137da
LongquanLiu <30406150+LongquanLiu@users.noreply.github.com>
2017-12-23 14:45:18 +0800

    Update merge-sorted-array.cpp

--- C++/merge-sorted-array.cpp
+++ C++/merge-sorted-array.cpp
@@ -1,7 +1,41 @@
 // Time:  O(m + n)
 // Space: O(1)
 
-class Solution {
+#include <vector>
+using namespace std;
+class Solution1 {
+public:
+    /*
+     * @param A: sorted integer array A
+     * @param B: sorted integer array B
+     * @return: A new sorted integer array
+     */
+    vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
+        // write your code here
+        vector<int> vec;
+        int index1 = 0, index2 = 0, len1 = A.size(), len2 = B.size();
+        while(index1 < len1 && index2 < len2) {
+            if(A[index1] < B[index2]){
+                vec.push_back(A[index1]);
+                index1++;
+            }else{
+                vec.push_back(B[index2]);
+                index2++;
+            }
+        }
+        while(index1 < len1){
+            vec.push_back(A[index1]);
+            index1++;
+        }
+        while(index2 < len2){
+            vec.push_back(B[index2]);
+            index2++;
+        }
+        return vec;
+    }
+};
+
+class Solution2 {
 public:
     /**
      * @param A: sorted integer array A which has m elements,
