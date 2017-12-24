66bfb68a0fdb5e7294d74e0a46b4edebb52826bb
LongquanLiu <30406150+LongquanLiu@users.noreply.github.com>
2017-12-24 09:50:57 +0800

    Update insert-interval.cpp

--- C++/insert-interval.cpp
+++ C++/insert-interval.cpp
@@ -1,41 +1,57 @@
 // Time:  O(n)
 // Space: O(1)
 
+#include <vector>
+using namespace std;
 /**
  * Definition of Interval:
- * classs Interval {
+ * class Interval {
+ * public:
  *     int start, end;
  *     Interval(int start, int end) {
  *         this->start = start;
  *         this->end = end;
  *     }
+ * }
  */
+class Interval {
+     public:
+         int start, end;
+        Interval(int start, int end) {
+                 this->start = start;
+                 this->end = end;
+        }
+};
+
 class Solution {
 public:
-    /**
-     * Insert newInterval into intervals.
+    /*
      * @param intervals: Sorted interval list.
      * @param newInterval: new interval.
      * @return: A new interval list.
      */
     vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        // write your code here
         size_t i = 0;
         vector<Interval> result;
-        // Insert intervals appeared before newInterval.
-        while (i < intervals.size() && newInterval.start > intervals[i].end) {
-            result.emplace_back(intervals[i++]);
+            // Insert intervals appeared before newInterval.
+        while(i < intervals.size() && intervals[i].end < newInterval.start){
+            result.emplace_back(intervals[i]);
+            i++;
         }
-
         // Merge intervals that overlap with newInterval.
-        while (i < intervals.size() && newInterval.end >= intervals[i].start) {
-            newInterval = {min(newInterval.start, intervals[i].start),
-                max(newInterval.end, intervals[i].end)};
-            ++i;
+        while(i < intervals.size() && intervals[i].start <= newInterval.end){
+            newInterval = {min(intervals[i].start, newInterval.start),
+                           max(intervals[i].end, newInterval.end)};
+            i++;
         }
         result.emplace_back(newInterval);
-
-        // Insert intervals appearing after newInterval.
-        result.insert(result.end(), intervals.cbegin() + i, intervals.cend());
+        // Insert intervals appearing after newInterval. 581ms
+        while(i < intervals.size() && intervals[i].start > newInterval.end){
+            result.emplace_back(intervals[i]);
+            i++;
+        }
+        // 689ms result.insert(result.end(), intervals.cbegin()+i, intervals.cend());
         return result;
     }
 };
